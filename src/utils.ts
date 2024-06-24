ee82d76db1ffa63655c47c486f88fb78cd79ec32
Artur Aronov <unionco@icloud.com>
2024-06-24 15:32:03 +0800

    utils: added compareTwoArrays

--- src/utils.ts
+++ src/utils.ts
@@ -34,5 +34,24 @@ export const generateRandomNumArr = (data: generateRandomNumArrT) => {
   return arr;
 };
 
+export const compareTwoArrays = (arr1: any[], arr2: any[]) => {
+  if (arr1.length !== arr2.length) {
+    console.log("Two arrays have mismatch length");
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(
+        `Arrays don't match in index ${i} array 1 value: ${arr1[i]} vs ${arr2[i]}`
+      );
+      return false;
+    }
+  }
+
+  console.log("Both arrays match");
+  return true;
+};
+
 // console.log(randomNumGenerator({}));
 // console.log(generateRandomNumArr({}));
