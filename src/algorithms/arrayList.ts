2b554bf6cabc2d63cf7abc318f5ce84b6fc845ae
Artur Aronov <unionco@icloud.com>
2024-07-09 12:06:15 +0800

    added array list

---
+++ src/algorithms/arrayList.ts
@@ -0,0 +1,47 @@
+export default class ArrayList<T> {
+  private length: number;
+  public array: Array<T | undefined> = new Array();
+
+  constructor() {
+    this.length = 3;
+    this.array = new Array(3);
+  }
+
+  add(item: T): void {
+    let availableIndex;
+    let low = 0;
+    let high = this.length;
+
+    while (low < high && !availableIndex) {
+      const mid = Math.floor((low + high) / 2);
+
+      if (!this.array[mid] && this.array[mid - 1]) {
+        availableIndex = mid;
+        break;
+      } else if (!this.array[mid]) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    if (!availableIndex) {
+      availableIndex = this.length;
+      const tempArray = this.array;
+      this.array = new Array(this.length * 2);
+      this.array = [...tempArray];
+    }
+
+    this.array[availableIndex] = item;
+    this.length++;
+  }
+
+  pop(): void {
+    this.array[length] = undefined;
+    this.length--;
+  }
+
+  get(index: number): T | undefined {
+    return this.array[index];
+  }
+}
